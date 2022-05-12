6b6fa89f92c525392fb9981b1073368811f1a598
Benjamin Brittain <ben@brittain.org>
2022-05-12 12:31:42 -0400

    Adds an example of using the aether server (6LowPan)

---
+++ task/aetherping/src/main.rs
@@ -0,0 +1,49 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![no_std]
+#![no_main]
+
+use task_aether_api::*;
+use userlib::*;
+
+task_slot!(AETHER, aether);
+
+#[export_name = "main"]
+fn main() -> ! {
+    let net = Aether::from(AETHER.get_task_id());
+
+    sys_log!("starting aetherping");
+    let tx = [0xBB, 0xBB, 0xBB];
+
+    let _meta = UdpMetadata {
+        // IPv6 multicast address for "all routers"
+        addr: Address::Ipv6(Ipv6Address([
+            0xff, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+        ])),
+        port: 8,
+        payload_len: tx.len() as u32,
+    };
+
+    let ip = net.get_addr();
+    sys_log!("generated an ip: {:?}", ip);
+
+    let mut rx_data_buf = [0u8; 128];
+    loop {
+        // sys_log!("SEND");
+        // net.send_packet(meta, &tx[..]);
+        // hl::sleep_for(4000);
+
+        sys_log!("RECV");
+        match net.recv_packet(&mut rx_data_buf) {
+            Ok(meta) => {
+                sys_log!("{:?}", &rx_data_buf[..meta.payload_len as usize]);
+            }
+            Err(AetherError::QueueEmpty) => {
+                // Our incoming queue is empty. Wait for more packets.
+                sys_recv_closed(&mut [], 5, TaskId::KERNEL).unwrap();
+            }
+        }
+    }
+}
