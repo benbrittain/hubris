eea00b3be1bfb7addc3e2095529f6493a925235b
Benjamin Brittain <ben@brittain.org>
2022-08-22 18:59:49 -0400

    Adds a 6LowPan network service
    * Adds a api dependency to radio, reconsider this structure

---
+++ task/aether/src/main.rs
@@ -0,0 +1,198 @@
+//! A 6LowPAN implementation over 802.15.4
+
+#![no_std]
+#![no_main]
+
+use drv_rng_api;
+use smoltcp::{
+    iface::{
+        FragmentsCache, InterfaceBuilder, Neighbor, NeighborCache,
+        PacketAssembler, SocketSet,
+    },
+    socket::{dns, udp},
+    time::Instant,
+    wire::{Ieee802154Address, IpAddress, IpCidr, SixlowpanFragKey},
+};
+use userlib::sys_log;
+
+// hacky way to get logs out of smoltcp
+#[cfg(feature = "log-smoltcp")]
+static SYS_LOGGER: log_smoltcp::SysLogger = log_smoltcp::SysLogger;
+
+userlib::task_slot!(RNG, rng_driver);
+
+#[cfg(feature = "log-smoltcp")]
+mod log_smoltcp;
+mod server;
+
+/// IEEE 802.15.4 link MTU
+const IEEE802_LINK_MTU: usize = 1280;
+
+/// Number of entries to maintain in our neighbor cache (ARP/NDP).
+const NEIGHBORS: usize = 4;
+
+/// Notification mask for our IRQ; must match configuration in app.toml.
+const RADIO_IRQ: u32 = 1;
+
+const MAX_FRAGMENTS: usize = 2;
+
+static mut PACKET_FRAGMENTS: [[u8; IEEE802_LINK_MTU]; MAX_FRAGMENTS] =
+    [[0; IEEE802_LINK_MTU]; MAX_FRAGMENTS];
+
+#[export_name = "main"]
+fn main() -> ! {
+    let rng = drv_rng_api::Rng::from(RNG.get_task_id());
+    // Setup a logger shim so we can see the output of smoltcp.
+    #[cfg(feature = "log-smoltcp")]
+    log::set_logger(&SYS_LOGGER).unwrap();
+    #[cfg(feature = "log-smoltcp")]
+    log::set_max_level(log::LevelFilter::Trace);
+
+    // Start up the radio.
+    let mut radio = nrf52_radio::Radio::new();
+    radio.initialize();
+
+    // Derive an IP address for our WPAN using IEEE UEI-64.
+    let ieee802154_addr: Ieee802154Address = radio.get_addr().into();
+    //let ieee802154_addr_short: Ieee802154Address =
+    //    smoltcp::wire::Ieee802154Address::Short([0x00, 0x08]);
+
+    // TODO We should set a link local address when we have SLAAC/NDISC working.
+    // let link_local_ipv6_addr =
+    //     IpAddress::Ipv6(ieee802154_addr.as_link_local_address().unwrap());
+
+    let mut site_local_ip_bytes = [0; 16];
+    // big endian so the ip addr looks pretty and like the pan_id
+    let pan_id_bytes = generated::PAN_ID.0.to_be_bytes();
+    site_local_ip_bytes[..8].copy_from_slice(&[
+        0xfd,
+        0x00,
+        pan_id_bytes[0],
+        pan_id_bytes[1],
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+    ]);
+    site_local_ip_bytes[8..].copy_from_slice(&radio.get_addr().0);
+    let site_local_ipv6_addr = IpAddress::Ipv6(
+        smoltcp::wire::Ipv6Address::from_bytes(&site_local_ip_bytes),
+    );
+    let mut ip_addrs = [
+        IpCidr::new(site_local_ipv6_addr, 64),
+        //        IpCidr::new(link_local_ipv6_addr, 64),
+    ];
+    for addr in ip_addrs {
+        sys_log!("IP addr: {}", addr);
+    }
+
+    let mut neighbor_cache_storage: [Option<(IpAddress, Neighbor)>; NEIGHBORS] =
+        [None; NEIGHBORS];
+    let neighbor_cache = NeighborCache::new(&mut neighbor_cache_storage[..]);
+
+    let mut packet_assembler_cache = unsafe {
+        [
+            PacketAssembler::<'_>::new(&mut PACKET_FRAGMENTS[0][..]),
+            PacketAssembler::<'_>::new(&mut PACKET_FRAGMENTS[1][..]),
+        ]
+    };
+    let mut packet_index_cache: [Option<(SixlowpanFragKey, usize)>;
+        MAX_FRAGMENTS] = [None; MAX_FRAGMENTS];
+    let fragments_cache = FragmentsCache::new(
+        &mut packet_assembler_cache[..],
+        &mut packet_index_cache[..],
+    );
+
+    let mut out_packet_buffer = [0u8; IEEE802_LINK_MTU];
+
+    let mut builder = InterfaceBuilder::new()
+        .ip_addrs(&mut ip_addrs[..])
+        .pan_id(generated::PAN_ID);
+    builder = builder
+        .hardware_addr(ieee802154_addr.into())
+        .neighbor_cache(neighbor_cache)
+        .sixlowpan_fragments_cache_timeout(smoltcp::time::Duration::from_secs(
+            2,
+        ))
+        .sixlowpan_fragments_cache(fragments_cache)
+        .sixlowpan_out_packet_cache(&mut out_packet_buffer[..]);
+    let iface = builder.finalize(&mut radio);
+
+    let mut socket_storage: [_; generated::SOCKET_COUNT + 1] =
+        Default::default();
+    let mut socket_set = SocketSet::new(&mut socket_storage[..]);
+
+    let sockets = generated::construct_sockets();
+    let mut socket_handles = [None; generated::SOCKET_COUNT + 1];
+    let mut socket_idx = 0;
+
+    // Udp sockets
+    for socket in sockets.udp {
+        socket_handles[socket_idx] =
+            Some(server::SocketHandleType::Udp(socket_set.add(socket)));
+        socket_idx += 1;
+    }
+
+    // Tcp sockets
+    for socket in sockets.tcp {
+        socket_handles[socket_idx] =
+            Some(server::SocketHandleType::Tcp(socket_set.add(socket)));
+        socket_idx += 1;
+    }
+
+    // Dns socket
+    let mut query_storage: [_; 2] = Default::default();
+    let dns_socket = dns::Socket::new(&[], &mut query_storage[..]);
+    socket_handles[socket_idx] =
+        Some(server::SocketHandleType::Dns(socket_set.add(dns_socket)));
+
+    let socket_handles = socket_handles.map(|h| h.unwrap());
+
+    // Bind sockets to their ports.
+    for (&handle, &port) in
+        socket_handles.iter().zip(&generated::UDP_SOCKET_PORTS)
+    {
+        match handle {
+            server::SocketHandleType::Udp(handle) => {
+                let udp_socket = socket_set.get_mut::<udp::Socket>(handle);
+                udp_socket.bind((site_local_ipv6_addr, port)).unwrap();
+            }
+            server::SocketHandleType::Tcp(_handle) => {}
+            server::SocketHandleType::Dns(_handle) => {}
+        }
+    }
+
+    userlib::sys_irq_control(RADIO_IRQ, true);
+    let mut server = server::AetherServer::new(
+        socket_handles,
+        socket_set,
+        iface,
+        radio,
+        rng,
+    );
+
+    loop {
+        let poll_result = server
+            .poll(Instant::from_millis(userlib::sys_get_timer().now as i64));
+        let activity = poll_result.unwrap_or(true);
+
+        if activity {
+            for i in 0..generated::SOCKET_COUNT {
+                // TODO check if there is a packet on the port
+                //if server.get_socket_mut(i).is_ok() {
+                let (task_id, notification) = generated::SOCKET_OWNERS[i];
+                let task_id = userlib::sys_refresh_task_id(task_id);
+                userlib::sys_post(task_id, notification);
+                //}
+            }
+            // TODO poll at timing perhaps?
+        } else {
+            let mut msgbuf = [0u8; server::INCOMING_SIZE];
+            idol_runtime::dispatch_n(&mut msgbuf, &mut server);
+        }
+    }
+}
+
+mod generated {
+    include!(concat!(env!("OUT_DIR"), "/aether_config.rs"));
+}
---
+++ task/aether/src/server.rs
@@ -0,0 +1,430 @@
+use crate::RADIO_IRQ;
+use idol_runtime::{ClientError, Leased, NotificationHandler, RequestError};
+use rand::Rng;
+use smoltcp::iface::{Interface, SocketHandle, SocketSet};
+use smoltcp::socket::dns::QueryHandle;
+use smoltcp::socket::{dns, tcp, udp};
+use smoltcp::wire::DnsQueryType;
+use task_aether_api::{
+    AetherError, Ieee802154Address, Ipv6Address, SocketName, TcpMetadata,
+    UdpMetadata,
+};
+
+use userlib::*;
+
+/// Size of buffer that must be allocated to use `dispatch`.
+pub const INCOMING_SIZE: usize = idl::INCOMING_SIZE;
+
+#[derive(Clone, Copy)]
+pub enum SocketHandleType {
+    Udp(SocketHandle),
+    Tcp(SocketHandle),
+    Dns(SocketHandle),
+}
+
+pub struct AetherServer<'a> {
+    socket_handles: [SocketHandleType; crate::generated::SOCKET_COUNT + 1],
+    socket_set: SocketSet<'a>,
+    iface: Interface<'a>,
+    device: nrf52_radio::Radio<'a>,
+    rng: drv_rng_api::Rng,
+    dns_query: Option<QueryHandle>,
+}
+
+impl<'a> AetherServer<'a> {
+    pub fn new(
+        socket_handles: [SocketHandleType; crate::generated::SOCKET_COUNT + 1],
+        socket_set: SocketSet<'a>,
+        iface: Interface<'a>,
+        device: nrf52_radio::Radio<'a>,
+        rng: drv_rng_api::Rng,
+    ) -> Self {
+        Self {
+            socket_handles,
+            socket_set,
+            iface,
+            device,
+            rng,
+            dns_query: None,
+        }
+    }
+
+    /// Poll the `smoltcp` `Interface`
+    pub fn poll(
+        &mut self,
+        time: smoltcp::time::Instant,
+    ) -> Result<bool, smoltcp::Error> {
+        self.iface
+            .poll(time, &mut self.device, &mut self.socket_set)
+    }
+
+    /// Gets the udp socket `index`. If `index` is out of range, returns
+    /// `BadMessage`. If the socket is not udp, error
+    pub fn get_udp_socket_mut(
+        &mut self,
+        index: usize,
+    ) -> Result<&mut udp::Socket<'a>, RequestError<AetherError>> {
+        let handle = self
+            .socket_handles
+            .get(index)
+            .cloned()
+            .ok_or(RequestError::Fail(ClientError::BadMessageContents))?;
+        match handle {
+            SocketHandleType::Udp(handle) => {
+                Ok(self.socket_set.get_mut::<udp::Socket>(handle))
+            }
+            _ => Err(AetherError::WrongSocketType.into()),
+        }
+    }
+    /// Gets the udp socket `index`. If `index` is out of range, returns
+    /// `BadMessage`. If the socket is not udp, error
+    pub fn get_tcp_socket_mut(
+        &mut self,
+        index: usize,
+    ) -> Result<&mut tcp::Socket<'a>, RequestError<AetherError>> {
+        let handle = self
+            .socket_handles
+            .get(index)
+            .cloned()
+            .ok_or(RequestError::Fail(ClientError::BadMessageContents))?;
+        match handle {
+            SocketHandleType::Tcp(handle) => {
+                Ok(self.socket_set.get_mut::<tcp::Socket>(handle))
+            }
+            _ => Err(AetherError::WrongSocketType.into()),
+        }
+    }
+}
+
+impl idl::InOrderAetherImpl for AetherServer<'_> {
+    fn recv_tcp_data(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        payload: Leased<idol_runtime::W, [u8]>,
+    ) -> Result<u32, RequestError<AetherError>> {
+        let socket_index = socket as usize;
+
+        if crate::generated::SOCKET_OWNERS[socket_index].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+        let socket = self.get_tcp_socket_mut(socket_index)?;
+        if socket.may_recv() {
+            match socket.recv(|data| (data.len(), data)) {
+                Ok(data) => {
+                    if data.len() == 0 {
+                        Err(AetherError::QueueEmpty.into())
+                    } else {
+                        if payload.len() < data.len() {
+                            return Err(RequestError::Fail(
+                                ClientError::BadLease,
+                            ));
+                        }
+                        payload
+                            .write_range(0..data.len(), data)
+                            .map_err(|_| RequestError::went_away())?;
+                        Ok(data.len() as u32)
+                    }
+                }
+                e => {
+                    sys_log!("got an unknown error: {:?}", e);
+                    Err(AetherError::Unknown.into())
+                }
+            }
+        } else if socket.may_send() {
+            Err(AetherError::RemoteTcpClose.into())
+        } else {
+            Err(AetherError::QueueEmpty.into())
+        }
+    }
+
+    fn tcp_listen(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        port: u16,
+    ) -> Result<(), RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_tcp_socket_mut(socket as usize)?;
+        // TODO consider using close and a seperate abort function?
+        socket.listen(port).map_err(|_| AetherError::Unknown.into())
+    }
+
+    fn tcp_connect(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        metadata: TcpMetadata,
+    ) -> Result<(), RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        // NOTE
+        // This doesn't use get_tcp_socket_mut because of double borrow problem
+        // This can probably be refactored to be cleaner
+        let remote_ep =
+            smoltcp::wire::IpEndpoint::from((metadata.addr, metadata.port));
+        let local_ep: u16 = self.rng.gen_range(1024..65535);
+        let handle = self
+            .socket_handles
+            .get(socket as usize)
+            .cloned()
+            .ok_or(RequestError::Fail(ClientError::BadMessageContents))?;
+        match handle {
+            SocketHandleType::Tcp(handle) => {
+                self.socket_set
+                    .get_mut::<tcp::Socket>(handle)
+                    .connect(self.iface.context(), remote_ep, local_ep)
+                    .map_err(|_| AetherError::TcpFailConnect)?;
+                Ok(())
+            }
+            _ => Err(AetherError::WrongSocketType.into()),
+        }
+    }
+
+    fn close_tcp(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+    ) -> Result<(), idol_runtime::RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_tcp_socket_mut(socket as usize)?;
+        // TODO consider using close and a seperate abort function?
+        socket.abort();
+        Ok(())
+    }
+
+    fn is_tcp_active(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+    ) -> Result<bool, idol_runtime::RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_tcp_socket_mut(socket as usize)?;
+        Ok(socket.may_send() && socket.may_recv())
+    }
+
+    fn send_tcp_data(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        payload: Leased<idol_runtime::R, [u8]>,
+    ) -> Result<u32, RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_tcp_socket_mut(socket as usize)?;
+
+        match socket.send(|buf| {
+            let len = if buf.len() < payload.len() {
+                buf.len()
+            } else {
+                payload.len()
+            };
+            match payload.read_range(0..payload.len(), buf) {
+                Ok(_) => (len, Ok(len as u32)),
+                Err(e) => (0, Err(e)),
+            }
+        }) {
+            Ok(Ok(len)) => Ok(len),
+            Ok(Err(_)) => Err(RequestError::Fail(ClientError::WentAway)),
+            Err(_) => Err(AetherError::SendError.into()),
+        }
+    }
+
+    fn recv_udp_packet(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        payload: Leased<idol_runtime::W, [u8]>,
+    ) -> Result<UdpMetadata, RequestError<AetherError>> {
+        let socket_index = socket as usize;
+
+        if crate::generated::SOCKET_OWNERS[socket_index].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_udp_socket_mut(socket_index)?;
+        match socket.recv() {
+            Ok((body, endp)) => {
+                if payload.len() < body.len() {
+                    return Err(RequestError::Fail(ClientError::BadLease));
+                }
+                payload
+                    .write_range(0..body.len(), body)
+                    .map_err(|_| RequestError::went_away())?;
+
+                Ok(UdpMetadata {
+                    port: endp.port,
+                    payload_len: body.len() as u32,
+                    addr: endp.addr.try_into().unwrap(),
+                })
+            }
+            Err(udp::RecvError::Exhausted) => {
+                Err(AetherError::QueueEmpty.into())
+            }
+        }
+    }
+
+    fn send_udp_packet(
+        &mut self,
+        msg: &userlib::RecvMessage,
+        socket: SocketName,
+        metadata: UdpMetadata,
+        payload: Leased<idol_runtime::R, [u8]>,
+    ) -> Result<(), RequestError<AetherError>> {
+        if crate::generated::SOCKET_OWNERS[socket as usize].0.index()
+            != msg.sender.index()
+        {
+            return Err(AetherError::WrongOwner.into());
+        }
+
+        let socket = self.get_udp_socket_mut(socket as usize)?;
+
+        match socket.send(payload.len(), metadata.into()) {
+            Ok(buf) => {
+                payload
+                    .read_range(0..payload.len(), buf)
+                    .map_err(|_| RequestError::went_away())?;
+                Ok(())
+            }
+            Err(udp::SendError::BufferFull) => {
+                Err(AetherError::NoTransmitSlot.into())
+            }
+            e => panic!("couldn't send packet {:?}", e),
+        }
+    }
+
+    fn get_addr(
+        &mut self,
+        _: &userlib::RecvMessage,
+    ) -> Result<Ieee802154Address, idol_runtime::RequestError<AetherError>>
+    {
+        Ok(self.device.get_addr())
+    }
+
+    fn get_rssi(
+        &mut self,
+        _: &userlib::RecvMessage,
+    ) -> Result<(), RequestError<AetherError>> {
+        unimplemented!();
+    }
+
+    fn resolve_query(
+        &mut self,
+        _: &userlib::RecvMessage,
+    ) -> Result<Ipv6Address, idol_runtime::RequestError<AetherError>> {
+        let dns_handle = match self.dns_query {
+            Some(handle) => handle,
+            None => return Err(AetherError::NoPendingDnsQuery.into()),
+        };
+
+        // We insert the DNS as the very last one
+        let dns_idx = self.socket_handles.len() - 1;
+        let handle = self
+            .socket_handles
+            .get(dns_idx)
+            .cloned()
+            .ok_or(RequestError::Fail(ClientError::BadMessageContents))?;
+
+        if let SocketHandleType::Dns(handle) = handle {
+            let dns = self.socket_set.get_mut::<dns::Socket>(handle);
+            match dns.get_query_result(dns_handle) {
+                Ok(names) => {
+                    let name = names.get(0).unwrap();
+                    match name {
+                        smoltcp::wire::IpAddress::Ipv6(ipv6) => {
+                            return Ok(Ipv6Address::from(*ipv6))
+                        }
+                    }
+                }
+                Err(dns::GetQueryResultError::Failed) => {
+                    return Err(AetherError::DnsFailure.into())
+                }
+                Err(dns::GetQueryResultError::Pending) => {
+                    return Err(AetherError::QueueEmpty.into())
+                }
+            }
+        }
+        panic!("Internal condiditon of DNS being last handle violated")
+    }
+
+    fn start_resolve_query(
+        &mut self,
+        _msg: &userlib::RecvMessage,
+        url: idol_runtime::Leased<idol_runtime::R, [u8]>,
+    ) -> Result<(), idol_runtime::RequestError<AetherError>> {
+        let mut url_buf = [0; 256];
+        url.read_range(0..url.len(), &mut url_buf[..url.len()])
+            .unwrap();
+        if self.dns_query.is_some() {
+            return Err(AetherError::DnsQueryAlreadyInflight.into());
+        }
+        // We insert the DNS as the very last one
+        let dns_idx = self.socket_handles.len() - 1;
+        let handle = self
+            .socket_handles
+            .get(dns_idx)
+            .cloned()
+            .ok_or(RequestError::Fail(ClientError::BadMessageContents))?;
+
+        if let SocketHandleType::Dns(handle) = handle {
+            let dns = self.socket_set.get_mut::<dns::Socket>(handle);
+            let handle = dns
+                .start_query(
+                    self.iface.context(),
+                    core::str::from_utf8(&url_buf[..url.len()]).unwrap(),
+                    DnsQueryType::Aaaa,
+                )
+                .map_err(|_| RequestError::went_away())?;
+            self.dns_query = Some(handle);
+            return Ok(());
+            // and make this totally blocking.
+        }
+        panic!("Internal condiditon of DNS being last handle violated")
+    }
+}
+
+impl NotificationHandler for AetherServer<'_> {
+    fn current_notification_mask(&self) -> u32 {
+        RADIO_IRQ
+    }
+
+    fn handle_notification(&mut self, bits: u32) {
+        // Interrupt dispatch.
+        if bits & RADIO_IRQ != 0 {
+            self.device.handle_interrupt();
+            userlib::sys_irq_control(RADIO_IRQ, true);
+        }
+    }
+}
+
+mod idl {
+    use task_aether_api::*;
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
+}
